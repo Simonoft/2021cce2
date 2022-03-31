77de1fde1e2ed290192d72dc9d4504239fb48831
Simonoft <32818902+Simonoft@users.noreply.github.com>
2022-03-31 16:17:45 +0800

    week06

---
+++ week06-1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char tree[100000][40];
+int compare(const void*p1,const void*p2){
+	char *s1=(char*)p1;
+	char *s2=(char*)p2;
+	return strcmp(s1,s2);
+}
+int main(){
+	int T;
+	scanf("%d\n\n",&T);
+	for(int t=1;t<=T;t++){
+		int n=0;
+		while(gets(tree[n])){
+			if(tree[n][0]==0){
+				break;
+			}
+			n++;
+		}
+		if(t>1) cout<<endl;
+		qsort(tree,n,40,compare);
+		for(int i=0;tree[i][0]!=0;i++){
+			cout<<tree[i]<<endl;
+		}
+	}
+}
+
