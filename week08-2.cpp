da366b9884739230ce971bdb3131dd69a3ffe22d
Simonoft <32818902+Simonoft@users.noreply.github.com>
2022-04-14 16:42:21 +0800

    week08

---
+++ week08-2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int grade[3];
+    for(int i=0;i<3;i++){
+        scanf("%d",&grade[i]);
+    }
+    for(int i=0;i<3;i++){
+        printf("%d\n",grade[i]);
+    }
+}
